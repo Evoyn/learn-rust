e9f65e7f66dadb6dc23d284dbd96d969df690781
Nebu <evoyniz@gmail.com>
2025-12-02 18:37:08 +0700

    day 1

--- variables-and-mutability/src/main.rs
+++ variables-and-mutability/src/main.rs
@@ -1,3 +1,15 @@
+// ini file level
+
+// Compailer Directives
+// #![allow(unused_variables)] //kalo ini global semua yang ada di file kena smua
+
+// Const
+const BIJI: i8 = 2;
+
+// Type Alias
+type Kilometer = i32;
+
+//[allow(unused_variables)] // bisa ditaruh sini ntar ngefek ke smua isi function
 fn main() {
     let apples: i32 = 50; // variable Immutable by default
     let mut oranges: i32 = 20; // add mut for mutable variable
@@ -25,4 +37,23 @@ fn main() {
     {
         println!("ini jeruk ada {}", oranges) // bisa akses dari luar scope, tapi yang diluar gabisa akses yg dalem
     }
+
+    //Constant > immutable by default and must be type annotated, bisa dipake di file level, variable harus huruf BESAR
+    println!("BIJI GW ADA {}", BIJI);
+
+    //Type Alias > liat di file level //fungsinya
+    let tinggi_gwej: Kilometer = 200;
+    let panjang_apadah_gatau: Kilometer = 3000;
+
+    println!(
+        "fungsinya biar rapi aja le ini type {}{}",
+        tinggi_gwej, panjang_apadah_gatau
+    )
+
+    // Compailer Directives
+    // #[] < ini syntaxnya
+    // #[allow(unused_variables)] //bisa ditaruh diatas function
+    // ini mirip //@TS-ignore
+    // bisa di file level jg
+    // selain allow banyak yg lain, sisanya cari sendiri
 }
