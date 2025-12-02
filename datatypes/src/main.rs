b39c82d96bb0106fdbf4e839b5ec6be2b6fcc310
Nebu <evoyniz@gmail.com>
2025-12-02 19:19:44 +0700

    day 1

--- datatypes/src/main.rs
+++ datatypes/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Data Types");
+
+    // scalar type int,float,bool,char
+
+    //signed and unsigned integer
+    // i8,u8,i16,u16,i32,u32,i64,u64,i128,u128,isize,usize
+    // signed support both positive and negative values "start with i"
+    // let a: i32 = -10;
+    // unsigned only support 0 to positive values "start with u", can store more data in positive range
+    // let c: u32 = 20;
+
+    //float
+    // f32 (6-9 digit dibelakang ,),f64 (15-17 digit dibelakang ,)
 }
